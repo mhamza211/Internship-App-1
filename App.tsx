8ac34a7ecb0711c47148fe5e9d320d8f7127c313
Muhammad Hamza <mhamza8732@gmail.com>
2026-06-10 20:39:46 +0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+            <Text style={styles.text}>This is my first project </Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
